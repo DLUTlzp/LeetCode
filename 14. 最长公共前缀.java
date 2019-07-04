9f3479fa5c96d5313d44b77037b31f6baf1fd5f7
DLUTlzp <37172489+DLUTlzp@users.noreply.github.com>
2019-07-04 18:50:40 +0800

    Add files via upload

---
+++ 14. 最长公共前缀.java
@@ -0,0 +1,46 @@
+// 编写一个函数来查找字符串数组中的最长公共前缀。
+// 如果不存在公共前缀，返回空字符串 ""。
+// 示例 1:
+// 输入: ["flower","flow","flight"]
+// 输出: "fl"
+// 示例 2:
+// 输入: ["dog","racecar","car"]
+// 输出: ""
+// 解释: 输入不存在公共前缀。
+// 说明:
+// 所有输入只包含小写字母 a-z 。
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/longest-common-prefix
+// 1.暴力求解
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0) 
+            return "";
+        String ans = strs[0];
+        for(int i =1;i<strs.length;i++) {
+            int j=0;
+            for(;j<ans.length() && j < strs[i].length();j++) {
+                if(ans.charAt(j) != strs[i].charAt(j))
+                    break;
+            }
+            ans = ans.substring(0, j);
+            if(ans.equals(""))
+                return ans;
+        }
+        return ans;
+    }
+}
+// 2.水平扫描法
+class Solution {
+public String longestCommonPrefix(String[] strs) {
+   if (strs.length == 0) return "";
+   String prefix = strs[0];
+   for (int i = 1; i < strs.length; i++)
+       while (strs[i].indexOf(prefix) != 0) {
+           prefix = prefix.substring(0, prefix.length() - 1);
+           if (prefix.isEmpty()) return "";
+       }        
+   return prefix;
+}
+}
+
