1e349d7e40a194b4a04ac8252152e909faedfc3e
DLUTlzp <37172489+DLUTlzp@users.noreply.github.com>
2019-07-15 18:42:30 +0800

    Add files via upload

---
+++ 19. 删除链表的倒数第N个节点.java
@@ -0,0 +1,24 @@
+//给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+//给定一个链表: 1->2->3->4->5, 和 n = 2.
+//当删除了倒数第二个节点后，链表变为 1->2->3->5.
+
+/**
+ * 时间复杂度O(N)，空间复杂度O(1)
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        for (int i = 1; i <= n + 1; i++) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+}
